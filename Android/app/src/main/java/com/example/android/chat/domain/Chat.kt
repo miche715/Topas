dd51dea4b528e81b8eb50a8d68b698fb470f6067
Cheel <44915367+miche715@users.noreply.github.com>
2022-07-24 18:20:27 +0900

    Chat에 timeStamp 프로퍼티 추가

--- Android/app/src/main/java/com/example/android/chat/domain/Chat.kt
+++ Android/app/src/main/java/com/example/android/chat/domain/Chat.kt
@@ -1,7 +1,10 @@
 package com.example.android.chat.domain
 
+import com.google.firebase.Timestamp
+
 data class Chat(var userDocumentId: String? = null,
                 var userNickName: String? = null,
                 var userProfilePhotoUri: String? = null,
-                var message: String = ""
+                var message: String = "",
+                var timeStamp: Timestamp? = null
 )
