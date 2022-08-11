79d8483fbc11846b9cb59a203a7fed97e25fe8db
ykm989 <ykm989@naver.com>
2022-08-12 00:56:39 +0900

    (2022.08.11) [Auth > SignUp] 비밀번호 확인 완성 및 이메일, 비밀번호 필터링 설정

--- ios/Topas/Topas/View/Auth/SignUpView.swift
+++ ios/Topas/Topas/View/Auth/SignUpView.swift
@@ -6,8 +6,184 @@
 //
 
 import UIKit
-/*
+
 class SignUpView: UIView{
+    let scrollView = UIScrollView().then{
+        $0.backgroundColor = .white
+        $0.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    let profileImage = UIImageView().then{
+        $0.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        //$0.layer.cornerRadius = $0.frame.width / 2
+        $0.clipsToBounds = true
+        $0.backgroundColor = .blue
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    let profileLabel = UILabel().then{
+        $0.text = "프로필 사진"
+        $0.textColor = .blue
+        $0.frame = CGRect(x: 5, y: 5, width: 100, height: 100)
+        
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    let emailLabel = UILabel().then{
+        $0.text = "* 이메일"
+    }
+    
+    let emailInput : UITextField = {
+        let view = UITextField()
+        
+        view.placeholder = "example@domain.com 형식의 이메일을 입력하세요."
+        view.textColor = .gray
+        view.autocapitalizationType = .none
+        
+        let underline = UIView().then{
+            $0.backgroundColor = .black
+        }
+        
+        view.addSubview(underline)
+        underline.snp.makeConstraints{
+            $0.width.equalTo(view.snp.width)
+            $0.bottom.equalTo(view.snp.bottom)
+            $0.height.equalTo(1)
+        }
+        
+        return view
+    }()
+    
+    let emailckLabel = UILabel()
+    
+    let pwLabel = UILabel().then{
+        $0.text = "* 패스워드"
+    }
+    
+    let pwInput : UITextField = {
+        let view = UITextField()
+        
+        view.placeholder = "4~20 자리 영어 대﹒소문자, 숫자, 특수문자 조합"
+        view.textColor = .gray
+        //view.isSecureTextEntry = true
+        view.textContentType = .password
+        view.autocapitalizationType = .none
+        
+        let underline = UIView().then{
+            $0.backgroundColor = .black
+        }
+        
+        view.addSubview(underline)
+        underline.snp.makeConstraints{
+            $0.width.equalTo(view.snp.width)
+            $0.bottom.equalTo(view.snp.bottom)
+            $0.height.equalTo(1)
+        }
+        
+        return view
+    }()
+    
+    let pwckLabel = UILabel()
+    
+    let pwreLabel = UILabel().then{
+        $0.text = "* 패스워드 확인"
+    }
+    
+    let pwreInput : UITextField = {
+        let view = UITextField()
+        
+        view.placeholder = "4~20 자리 영어 대﹒소문자, 숫자, 특수문자 조합"
+        view.textColor = .gray
+        //view.isSecureTextEntry = true
+        view.textContentType = .password
+        view.autocapitalizationType = .none
+        
+        let underline = UIView().then{
+            $0.backgroundColor = .black
+        }
+        
+        view.addSubview(underline)
+        underline.snp.makeConstraints{
+            $0.width.equalTo(view.snp.width)
+            $0.bottom.equalTo(view.snp.bottom)
+            $0.height.equalTo(1)
+        }
+        return view
+    }()
+    
+    let pwreckLabel = UILabel()
+    
+    let nameLabel = UILabel().then{
+        $0.text = "* 이름"
+    }
+    
+    let nameInput : UITextField = {
+        let view = UITextField()
+        
+        view.placeholder = "한글로 이루어진 이름"
+        view.textColor = .gray
+        
+        let underline = UIView().then{
+            $0.backgroundColor = .black
+        }
+        
+        view.addSubview(underline)
+        underline.snp.makeConstraints{
+            $0.width.equalTo(view.snp.width)
+            $0.bottom.equalTo(view.snp.bottom)
+            $0.height.equalTo(1)
+        }
+        
+        return view
+    }()
+    
+    let nameckLabel = UILabel()
+    
+    let nicknameLabel = UILabel().then{
+        $0.text = "* 닉네임"
+    }
+    
+    let nicknameInput : UITextField = {
+        let view = UITextField()
+        
+        view.placeholder = "2~8 자리 한글, 영어 대﹒소문자, 숫자 조합"
+        view.textColor = .gray
+        view.autocapitalizationType = .none
+        
+        let underline = UIView().then{
+            $0.backgroundColor = .black
+        }
+        
+        view.addSubview(underline)
+        underline.snp.makeConstraints{
+            $0.width.equalTo(view.snp.width)
+            $0.bottom.equalTo(view.snp.bottom)
+            $0.height.equalTo(1)
+        }
+        
+        return view
+    }()
+    
+    let nicknameckLabel = UILabel()
+    
+    let SignUpbutton = UIButton().then{
+        $0.setTitle("회원가입", for: .normal)
+        $0.setTitleColor(UIColor.white, for: .normal)
+        $0.backgroundColor = #colorLiteral(red: 0.002839220921, green: 0.4000240564, blue: 1, alpha: 1)
+        
+        $0.layer.shadowColor = UIColor.black.cgColor
+        $0.layer.masksToBounds = false
+        $0.layer.shadowOffset = CGSize(width: 0, height: 4)
+        $0.layer.shadowOpacity = 0.3
+        
+        $0.layer.cornerRadius = 6
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.black.cgColor
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -20,7 +196,133 @@ class SignUpView: UIView{
     
     private func commonInit(){
         self.backgroundColor = .white
+        self.addSubview(scrollView)
+        
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.widthAnchor.constraint(equalTo: self.safeAreaLayoutGuide.widthAnchor)
+        ])
+        
+        scrollView.addSubview(profileImage)
+        profileImage.snp.makeConstraints{ make in
+            make.top.equalTo(scrollView.snp.top).offset(100)
+            make.centerX.equalTo(scrollView)
+            //make.width.equalTo(scrollView).multipliedBy(0.2)
+            make.width.height.equalTo(100)
+        }
+        
+        scrollView.addSubview(profileLabel)
+        profileLabel.snp.makeConstraints{ make in
+            make.top.equalTo(profileImage.snp.bottom).offset(20)
+            make.centerX.equalTo(profileImage)
+        }
+        
+        scrollView.addSubview(emailLabel)
+        emailLabel.snp.makeConstraints{
+            $0.top.equalTo(profileLabel.snp.bottom).offset(50)
+            $0.leading.equalTo(scrollView.snp.leading).offset(10)
+            
+        }
+        
+        scrollView.addSubview(emailInput)
+        emailInput.snp.makeConstraints{ make in
+            make.top.equalTo(emailLabel.snp.bottom).offset(10)
+            make.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
+            make.centerX.equalTo(profileLabel)
+        }
+        
+        scrollView.addSubview(emailckLabel)
+        emailckLabel.snp.makeConstraints{
+            $0.top.equalTo(emailInput.snp.bottom).offset(2)
+            $0.leading.equalTo(emailInput.snp.leading)
+        }
+        
+        scrollView.addSubview(pwLabel)
+        pwLabel.snp.makeConstraints{
+            $0.top.equalTo(emailInput.snp.bottom).offset(30)
+            $0.leading.equalTo(scrollView.snp.leading).offset(10)
+        }
+        
+        scrollView.addSubview(pwInput)
+        pwInput.snp.makeConstraints{
+            $0.top.equalTo(pwLabel.snp.bottom).offset(10)
+            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
+            $0.centerX.equalTo(emailInput)
+        }
+        
+        scrollView.addSubview(pwckLabel)
+        pwckLabel.snp.makeConstraints{
+            $0.top.equalTo(pwInput.snp.bottom).offset(2)
+            $0.leading.equalTo(pwInput.snp.leading)
+        }
+        
+        scrollView.addSubview(pwreLabel)
+        pwreLabel.snp.makeConstraints{
+            $0.top.equalTo(pwInput.snp.bottom).offset(30)
+            $0.leading.equalTo(scrollView.snp.leading).offset(10)
+        }
+        
+        scrollView.addSubview(pwreInput)
+        pwreInput.snp.makeConstraints{
+            $0.top.equalTo(pwreLabel.snp.bottom).offset(10)
+            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
+            $0.centerX.equalTo(pwInput)
+        }
+        
+        scrollView.addSubview(pwreckLabel)
+        pwreckLabel.snp.makeConstraints{
+            $0.top.equalTo(pwreInput.snp.bottom).offset(2)
+            $0.leading.equalTo(pwreInput.snp.leading)
+        }
+        
+        scrollView.addSubview(nameLabel)
+        nameLabel.snp.makeConstraints{
+            $0.top.equalTo(pwreInput.snp.bottom).offset(30)
+            $0.leading.equalTo(scrollView.snp.leading).offset(10)
+        }
+        
+        scrollView.addSubview(nameInput)
+        nameInput.snp.makeConstraints{
+            $0.top.equalTo(nameLabel.snp.bottom).offset(10)
+            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
+            $0.centerX.equalTo(pwreInput)
+        }
+        
+        scrollView.addSubview(nameckLabel)
+        nameckLabel.snp.makeConstraints{
+            $0.top.equalTo(nameInput.snp.bottom).offset(2)
+            $0.leading.equalTo(nameInput.snp.leading)
+        }
+        
+        scrollView.addSubview(nicknameLabel)
+        nicknameLabel.snp.makeConstraints{
+            $0.top.equalTo(nameInput.snp.bottom).offset(30)
+            $0.leading.equalTo(scrollView.snp.leading).offset(10)
+        }
+        
+        scrollView.addSubview(nicknameInput)
+        nicknameInput.snp.makeConstraints{
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(10)
+            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
+            $0.centerX.equalTo(nameInput)
+        }
+        
+        scrollView.addSubview(nicknameckLabel)
+        nicknameckLabel.snp.makeConstraints{
+            $0.top.equalTo(nicknameInput.snp.bottom).offset(2)
+            $0.leading.equalTo(nicknameInput.snp.leading)
+        }
+        
+        scrollView.addSubview(SignUpbutton)
+        SignUpbutton.snp.makeConstraints{
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(50)
+            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
+            $0.centerX.equalTo(nicknameInput)
+        }
         
     }
 }
-*/
--- ios/Topas/Topas/ViewController/Auth/SignUpViewController.swift
+++ ios/Topas/Topas/ViewController/Auth/SignUpViewController.swift
@@ -20,6 +20,15 @@ class SignUp: UIViewController{
         self.view = vc
         profilePickController.delegate = self
         imageaddGestureRecognizer()
+        
+        vc.emailInput.delegate = self
+        vc.pwInput.delegate = self
+        vc.pwreInput.delegate = self
+        vc.nicknameInput.delegate = self
+        vc.nameInput.delegate = self
+        
+        // 패스워드 확인 바뀌었을 떄 값 체크하는 메소드를 추가해준다.
+        vc.pwreInput.addTarget(self, action: #selector(setLabelPasswordConfirm(_:)), for: .editingChanged)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -82,321 +91,131 @@ class SignUp: UIViewController{
     
     
 }
-
 extension SignUp : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     
 }
 
-
-
-
-
-struct ViewControllerRepresentable: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> SignUp {
-        return SignUp()
+extension SignUp: UITextFieldDelegate{
+    // 패스워드가 같은지 다른지 확인 후 라벨의 글자를 바꿔준다.
+    @objc func setLabelPasswordConfirm(_ sender:Any?){
+        let passwordConfirm = vc.pwInput.text
+        let password = vc.pwreInput.text
+        guard passwordConfirm != "" else {
+            vc.pwreckLabel.text = ""
+            return
+        }
+        
+        if password == passwordConfirm{
+            vc.pwreckLabel.text = "패스워드가 일치합니다."
+            vc.pwreckLabel.textColor = .green
+        } else {
+            vc.pwreckLabel.text = "패스워드가 다릅니다."
+            vc.pwreckLabel.textColor = .red
+        }
     }
     
-    func updateUIViewController(_ uiViewController: SignUp, context: Context) {
+    //Text Field에서 Enter 눌렀을 때 포커싱 옮기기
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField{
+        case vc.emailInput:
+            // 이메일 -> 비밀번호
+            vc.pwInput.becomeFirstResponder()
+        case vc.pwInput:
+            // 비밀번호 -> 비밀번호 재입력
+            vc.pwreInput.becomeFirstResponder()
+        case vc.pwreInput:
+            // 비밀번호 재입력 -> 이름
+            vc.nameInput.becomeFirstResponder()
+        case vc.nameInput:
+            // 이름 -> 닉네임
+            vc.nicknameInput.becomeFirstResponder()
+        default:
+            // 닉네임 -> 회원가입
+            vc.SignUpbutton.becomeFirstResponder()
+        }
+        return false
     }
     
+    // 키보드에서 사용자가 키 하나를 눌렀을 때 취해야 할 동작을 지정
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let utf8Char = string.cString(using: .utf8)
+        let isBackSpace = strcmp(utf8Char, "\\b")
+        
+        switch textField{
+        case vc.emailInput:
+            if string.emailCheck() || isBackSpace == -92 {
+                return true
+            }
+            return false
+        case vc.pwInput:
+            if string.pwCheck() || isBackSpace == -92 {
+                return true
+            }
+            return false
+        case vc.pwreInput:
+            print("hello")
+        case vc.nameInput:
+            print("nameInput")
+        default:
+            print("nicknameInput")
+        }
+        return true
+        
+    }
     
-    typealias UIViewControllerType = SignUp
 
 }
-
-@available(iOS 15.5, *)
-struct ViewPreview: PreviewProvider {
-    static var previews: some View {
-        ViewControllerRepresentable()
-    }
-}
-
-class SignUpView: UIView{
-    let scrollView = UIScrollView().then{
-        $0.backgroundColor = .white
-        $0.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
-        
-        $0.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    let profileImage = UIImageView().then{
-        $0.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
-        //$0.layer.cornerRadius = $0.frame.width / 2
-        $0.clipsToBounds = true
-        $0.backgroundColor = .blue
-        $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.contentMode = .scaleAspectFill
-    }
-    
-    let profileLabel = UILabel().then{
-        $0.text = "프로필 사진"
-        $0.textColor = .blue
-        $0.frame = CGRect(x: 5, y: 5, width: 100, height: 100)
-        
-        $0.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    let emailLabel = UILabel().then{
-        $0.text = "* 이메일"
-    }
-    
-    /*
-    let emailInput = UITextField().then{
-        $0.placeholder = "example@domain.com 형식의 이메일을 입력하세요."
-        $0.textColor = .gray
-        
-    }*/
-    
-    let emailInput : UITextField = {
-        let view = UITextField()
-        
-        view.placeholder = "example@domain.com 형식의 이메일을 입력하세요."
-        view.textColor = .gray
-        
-        let underline = UIView().then{
-            $0.backgroundColor = .black
-        }
-        
-        view.addSubview(underline)
-        underline.snp.makeConstraints{
-            $0.width.equalTo(view.snp.width)
-            $0.bottom.equalTo(view.snp.bottom)
-            $0.height.equalTo(1)
-        }
-        
-        return view
-    }()
-    
-    let pwLabel = UILabel().then{
-        $0.text = "* 패스워드"
-    }
-    
-    let pwInput : UITextField = {
-        let view = UITextField()
-        
-        view.placeholder = "4~20 자리 영어 대﹒소문자, 숫자, 특수문자 조합"
-        view.textColor = .gray
-        view.isSecureTextEntry = true
-        view.textContentType = .password
-        view.autocapitalizationType = .none
-        
-        let underline = UIView().then{
-            $0.backgroundColor = .black
-        }
-        
-        view.addSubview(underline)
-        underline.snp.makeConstraints{
-            $0.width.equalTo(view.snp.width)
-            $0.bottom.equalTo(view.snp.bottom)
-            $0.height.equalTo(1)
-        }
-        
-        return view
-    }()
-    
-    let pwreLabel = UILabel().then{
-        $0.text = "* 패스워드 확인"
-    }
-    
-    let pwreInput : UITextField = {
-        let view = UITextField()
-        
-        view.placeholder = "4~20 자리 영어 대﹒소문자, 숫자, 특수문자 조합"
-        view.textColor = .gray
-        view.isSecureTextEntry = true
-        view.textContentType = .password
-        view.autocapitalizationType = .none
-        
-        let underline = UIView().then{
-            $0.backgroundColor = .black
-        }
-        
-        view.addSubview(underline)
-        underline.snp.makeConstraints{
-            $0.width.equalTo(view.snp.width)
-            $0.bottom.equalTo(view.snp.bottom)
-            $0.height.equalTo(1)
-        }
-        return view
-    }()
-    
-    let nameLabel = UILabel().then{
-        $0.text = "* 이름"
-    }
-    
-    let nameInput : UITextField = {
-        let view = UITextField()
-        
-        view.placeholder = "한글로 이루어진 이름"
-        view.textColor = .gray
-        
-        let underline = UIView().then{
-            $0.backgroundColor = .black
-        }
-        
-        view.addSubview(underline)
-        underline.snp.makeConstraints{
-            $0.width.equalTo(view.snp.width)
-            $0.bottom.equalTo(view.snp.bottom)
-            $0.height.equalTo(1)
-        }
-        
-        return view
-    }()
-    
-    let nicknameLabel = UILabel().then{
-        $0.text = "* 닉네임"
-    }
-    
-    let nicknameInput : UITextField = {
-        let view = UITextField()
-        
-        view.placeholder = "2~8 자리 한글, 영어 대﹒소문자, 숫자 조합"
-        view.textColor = .gray
-        
-        let underline = UIView().then{
-            $0.backgroundColor = .black
-        }
-        
-        view.addSubview(underline)
-        underline.snp.makeConstraints{
-            $0.width.equalTo(view.snp.width)
-            $0.bottom.equalTo(view.snp.bottom)
-            $0.height.equalTo(1)
-        }
-        
-        return view
-    }()
-    
-    
-    let SignUpbutton = UIButton().then{
-        $0.setTitle("회원가입", for: .normal)
-        $0.setTitleColor(UIColor.white, for: .normal)
-        $0.backgroundColor = #colorLiteral(red: 0.002839220921, green: 0.4000240564, blue: 1, alpha: 1)
-        
-        $0.layer.shadowColor = UIColor.black.cgColor
-        $0.layer.masksToBounds = false
-        $0.layer.shadowOffset = CGSize(width: 0, height: 4)
-        $0.layer.shadowOpacity = 0.3
-        
-        $0.layer.cornerRadius = 6
-        $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor.black.cgColor
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        commonInit()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        commonInit()
-    }
-    
-    private func commonInit(){
-        self.backgroundColor = .white
-        self.addSubview(scrollView)
-        
-        
-        NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
-            scrollView.widthAnchor.constraint(equalTo: self.safeAreaLayoutGuide.widthAnchor)
-        ])
-        
-        
-        
-        
-        scrollView.addSubview(profileImage)
-        profileImage.snp.makeConstraints{ make in
-            make.top.equalTo(scrollView.snp.top).offset(100)
-            make.centerX.equalTo(scrollView)
-            //make.width.equalTo(scrollView).multipliedBy(0.2)
-            make.width.height.equalTo(100)
-        }
-        
-        scrollView.addSubview(profileLabel)
-        profileLabel.snp.makeConstraints{ make in
-            make.top.equalTo(profileImage.snp.bottom).offset(20)
-            make.centerX.equalTo(profileImage)
-        }
-        
-        scrollView.addSubview(emailLabel)
-        emailLabel.snp.makeConstraints{
-            $0.top.equalTo(profileLabel.snp.bottom).offset(50)
-            $0.leading.equalTo(scrollView.snp.leading).offset(10)
+extension String{
+    // 필터링을 위함
+    func emailCheck() -> Bool {
+        do{
             
+            let regex = try NSRegularExpression(pattern: "^[a-zA-Z1-9@.\\s]$", options: .caseInsensitive)
+            if let _ = regex.firstMatch(in: self,options: NSRegularExpression.MatchingOptions.reportProgress, range: NSMakeRange(0, self.count)){
+                return true
+            }
+        }catch{
+            return false
         }
-        
-        scrollView.addSubview(emailInput)
-        emailInput.snp.makeConstraints{ make in
-            make.top.equalTo(emailLabel.snp.bottom).offset(10)
-            make.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
-            make.centerX.equalTo(profileLabel)
+        return false
+    }
+    
+    func pwCheck() -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "^[a-zA-Z1-9@#$%^&*.!?\\s]$", options: .caseInsensitive)
+            if let _ = regex.firstMatch(in: self,options: NSRegularExpression.MatchingOptions.reportProgress, range: NSMakeRange(0, self.count)){
+                return true
+            }
+        }catch{
+            return false
         }
-        
-        scrollView.addSubview(pwLabel)
-        pwLabel.snp.makeConstraints{
-            $0.top.equalTo(emailInput.snp.bottom).offset(30)
-            $0.leading.equalTo(scrollView.snp.leading).offset(10)
-        }
-        
-        scrollView.addSubview(pwInput)
-        pwInput.snp.makeConstraints{
-            $0.top.equalTo(pwLabel.snp.bottom).offset(10)
-            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
-            $0.centerX.equalTo(emailInput)
-        }
-        
-        scrollView.addSubview(pwreLabel)
-        pwreLabel.snp.makeConstraints{
-            $0.top.equalTo(pwInput.snp.bottom).offset(30)
-            $0.leading.equalTo(scrollView.snp.leading).offset(10)
-        }
-        
-        scrollView.addSubview(pwreInput)
-        pwreInput.snp.makeConstraints{
-            $0.top.equalTo(pwreLabel.snp.bottom).offset(10)
-            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
-            $0.centerX.equalTo(pwInput)
-        }
-        
-        scrollView.addSubview(nameLabel)
-        nameLabel.snp.makeConstraints{
-            $0.top.equalTo(pwreInput.snp.bottom).offset(30)
-            $0.leading.equalTo(scrollView.snp.leading).offset(10)
-        }
-        
-        scrollView.addSubview(nameInput)
-        nameInput.snp.makeConstraints{
-            $0.top.equalTo(nameLabel.snp.bottom).offset(10)
-            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
-            $0.centerX.equalTo(pwreInput)
-        }
-        
-        scrollView.addSubview(nicknameLabel)
-        nicknameLabel.snp.makeConstraints{
-            $0.top.equalTo(nameInput.snp.bottom).offset(30)
-            $0.leading.equalTo(scrollView.snp.leading).offset(10)
-        }
-        
-        scrollView.addSubview(nicknameInput)
-        nicknameInput.snp.makeConstraints{
-            $0.top.equalTo(nicknameLabel.snp.bottom).offset(10)
-            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
-            $0.centerX.equalTo(nameInput)
-        }
-        
-        scrollView.addSubview(SignUpbutton)
-        SignUpbutton.snp.makeConstraints{
-            $0.top.equalTo(nicknameLabel.snp.bottom).offset(50)
-            $0.width.equalTo(scrollView.snp.width).multipliedBy(0.8)
-            $0.centerX.equalTo(nicknameInput)
-        }
-        
+        return false
     }
 }
+
+
+
+
+//struct ViewControllerRepresentable: UIViewControllerRepresentable {
+//    func makeUIViewController(context: Context) -> SignUp {
+//        return SignUp()
+//    }
+//
+//    func updateUIViewController(_ uiViewController: SignUp, context: Context) {
+//    }
+//
+//
+//    typealias UIViewControllerType = SignUp
+//
+//}
+//
+//@available(iOS 15.5, *)
+//struct ViewPreview: PreviewProvider {
+//    static var previews: some View {
+//        ViewControllerRepresentable()
+//    }
+//}
+//
+//
