343533ea6822bdf955c11f9c1af5ebfaf0c580e1
ykm989 <ykm989@naver.com>
2022-08-19 18:52:46 +0900

    온보딩 완성

--- ios/Topas/Topas/View/OnBoard/OnBoardView.swift
+++ ios/Topas/Topas/View/OnBoard/OnBoardView.swift
@@ -5,6 +5,7 @@
 //  Created by 김경호 on 2022/07/25.
 //
 import UIKit
+import SnapKit
 
 class onboard1: UIView{
     override init(frame: CGRect) {
@@ -19,52 +20,60 @@ class onboard1: UIView{
     
     let vc = UIView()
     
-    let title: UILabel = {
-        let label = UILabel()
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "팀을 찾고 계십니까?"
-        label.frame = CGRect(x: 30, y: 120, width: 250, height: 50)
-        label.font = UIFont.boldSystemFont(ofSize: 30)
-        label.textColor = UIColor.white
-        return label
-    }()
+    let title = UILabel().then{
+        $0.text = "팀을 찾고 계십니까?"
+        $0.font = UIFont.boldSystemFont(ofSize: 30)
+        $0.textColor = UIColor.white
+    }
     
     
-    let des1 : UILabel = {
-        let label = UILabel()
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Topas에는"
-        label.frame = CGRect(x: 30, y: 180, width: 150, height: 50)
-        label.font = UIFont.systemFont(ofSize: 25)
-        label.textColor = UIColor.white
-        return label
-    }()
+    let des1 = UILabel().then{
+        $0.text = "Topas에는"
+        $0.font = UIFont.systemFont(ofSize: 25)
+        $0.textColor = UIColor.white
+    }
     
-    let des2 : UILabel = {
-        let label = UILabel()
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "수 많은 팀들이"
-        label.frame = CGRect(x: 30, y: 210, width: 150, height: 50)
-        label.font = UIFont.systemFont(ofSize: 25)
-        label.textColor = UIColor.white
-        return label
-    }()
+    let des2 = UILabel().then{
+        $0.text = "수 많은 팀들이"
+        $0.font = UIFont.systemFont(ofSize: 25)
+        $0.textColor = UIColor.white
+    }
     
-    let des3 : UILabel = {
-        let label = UILabel()
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "당신을 위해 기다립니다."
-        label.frame = CGRect(x: 30, y: 240, width: 300, height: 50)
-        label.font = UIFont.systemFont(ofSize: 25)
-        label.textColor = UIColor.white
-        return label
-    }()
+    let des3 = UILabel().then{
+        $0.text = "당신을 위해 기다립니다."
+        $0.font = UIFont.systemFont(ofSize: 25)
+        $0.textColor = UIColor.white
+    }
     
     private func commonInit(){
         self.addSubview(title)
+        title.snp.makeConstraints{
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(70)
+            $0.width.equalTo(300)
+            $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
+            $0.height.equalTo(50)
+        }
+        
         self.addSubview(des1)
+        des1.snp.makeConstraints{
+            $0.top.equalTo(title.snp.bottom).offset(20)
+            $0.width.equalTo(title.snp.width)
+            $0.leading.equalTo(title.snp.leading)
+        }
+        
         self.addSubview(des2)
+        des2.snp.makeConstraints{
+            $0.top.equalTo(des1.snp.bottom)
+            $0.width.equalTo(des1.snp.width)
+            $0.leading.equalTo(des1.snp.leading)
+        }
+        
         self.addSubview(des3)
+        des3.snp.makeConstraints{
+            $0.top.equalTo(des2.snp.bottom)
+            $0.width.equalTo(des2.snp.width)
+            $0.leading.equalTo(des2.snp.leading)
+        }
     }
 }
 
@@ -82,51 +91,59 @@ class onboard2: UIView{
     
     let vc = UIView()
     
-    let title: UILabel = {
-        let label = UILabel()
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "팀원을 찾고 계십니까?"
-        label.frame = CGRect(x: 30, y: 120, width: 300, height: 50)
-        label.font = UIFont.boldSystemFont(ofSize: 30)
-        label.textColor = UIColor.white
-        return label
-    }()
+    let title = UILabel().then{
+        $0.text = "팀원을 찾고 계십니까?"
+        $0.font = UIFont.boldSystemFont(ofSize: 30)
+        $0.textColor = UIColor.white
+    }
     
-    let des1 : UILabel = {
-        let label = UILabel()
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Topas에는"
-        label.frame = CGRect(x: 30, y: 180, width: 200, height: 50)
-        label.font = UIFont.systemFont(ofSize: 25)
-        label.textColor = UIColor.white
-        return label
-    }()
+    let des1 = UILabel().then{
+        $0.text = "Topas에는"
+        $0.font = UIFont.systemFont(ofSize: 25)
+        $0.textColor = UIColor.white
+    }
     
-    let des2 : UILabel = {
-        let label = UILabel()
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "수 많은 사람들이"
-        label.frame = CGRect(x: 30, y: 210, width: 200, height: 50)
-        label.font = UIFont.systemFont(ofSize: 25)
-        label.textColor = UIColor.white
-        return label
-    }()
+    let des2 = UILabel().then{
+        $0.text = "수 많은 사람들이"
+        $0.font = UIFont.systemFont(ofSize: 25)
+        $0.textColor = UIColor.white
+    }
     
-    let des3 : UILabel = {
-        let label = UILabel()
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "당신을 위해 기다립니다."
-        label.frame = CGRect(x: 30, y: 240, width: 300, height: 50)
-        label.font = UIFont.systemFont(ofSize: 25)
-        label.textColor = UIColor.white
-        return label
-    }()
+    let des3 = UILabel().then{
+        $0.text = "당신을 위해 기다립니다."
+        $0.font = UIFont.systemFont(ofSize: 25)
+        $0.textColor = UIColor.white
+    }
     
     private func commonInit(){
         self.addSubview(title)
+        title.snp.makeConstraints{
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(70)
+            $0.width.equalTo(300)
+            $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
+            $0.height.equalTo(50)
+        }
+        
         self.addSubview(des1)
+        des1.snp.makeConstraints{
+            $0.top.equalTo(title.snp.bottom).offset(20)
+            $0.width.equalTo(title.snp.width)
+            $0.leading.equalTo(title.snp.leading)
+        }
+        
         self.addSubview(des2)
+        des2.snp.makeConstraints{
+            $0.top.equalTo(des1.snp.bottom)
+            $0.width.equalTo(des1.snp.width)
+            $0.leading.equalTo(des1.snp.leading)
+        }
+        
         self.addSubview(des3)
+        des3.snp.makeConstraints{
+            $0.top.equalTo(des2.snp.bottom)
+            $0.width.equalTo(des2.snp.width)
+            $0.leading.equalTo(des2.snp.leading)
+        }
     }
 }
 
@@ -143,48 +160,35 @@ class onboard3: UIView{
     
     let vc = UIView()
     
-    let title: UILabel = {
-        let label = UILabel()
-        //label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Topas 지금 시작하세요."
-        label.frame = CGRect(x: 30, y: 120, width: 300, height: 50)
-        label.font = UIFont.boldSystemFont(ofSize: 30)
-        label.textColor = UIColor.white
-        return label
-    }()
+    let title = UILabel().then{
+        $0.text = "Topas 지금 시작하세요."
+        $0.font = UIFont.boldSystemFont(ofSize: 30)
+        $0.textColor = UIColor.white
+    }
     
     
-    let signupButton: UIButton = {
-        let button = UIButton()
-        //button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("새로운 회원으로 가입하기", for: .normal)
-        button.setTitleColor(UIColor.black, for: .normal)
-        button.frame = CGRect(x: 40, y: 650, width: 350, height: 40)
-        button.backgroundColor = .white
-        button.setTitleColor(#colorLiteral(red: 0.002839220921, green: 0.4000240564, blue: 1, alpha: 1), for: .normal)
+    let signupButton = UIButton().then{
+        $0.setTitle("새로운 회원으로 가입하기", for: .normal)
+        $0.setTitleColor(UIColor.black, for: .normal)
+        $0.backgroundColor = .white
+        $0.setTitleColor(UIColor(named: "BrandColor"), for: .normal)
         //그림자
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.masksToBounds = false
-        button.layer.shadowOffset = CGSize(width: 0, height: 4)
-        button.layer.shadowOpacity = 0.3
-        return button
-    }()
+        $0.layer.shadowColor = UIColor.black.cgColor
+        $0.layer.masksToBounds = false
+        $0.layer.shadowOffset = CGSize(width: 0, height: 4)
+        $0.layer.shadowOpacity = 0.3
+    }
     
-    let signinButton: UIButton = {
-        let button = UIButton()
-        //button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("기존 회원으로 시작하기", for: .normal)
-        button.setTitleColor(#colorLiteral(red: 0.002839220921, green: 0.4000240564, blue: 1, alpha: 1), for: .normal)
-        button.frame = CGRect(x: 40, y: 700, width: 350, height: 40)
-        button.backgroundColor = .white
-        button.setTitleColor(#colorLiteral(red: 0.002839220921, green: 0.4000240564, blue: 1, alpha: 1), for: .normal)
+    let signinButton = UIButton().then{
+        $0.setTitle("기존 회원으로 시작하기", for: .normal)
+        $0.setTitleColor(UIColor(named: "BrandColor"), for: .normal)
+        $0.backgroundColor = .white
         //그림자
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.masksToBounds = false
-        button.layer.shadowOffset = CGSize(width: 0, height: 4)
-        button.layer.shadowOpacity = 0.3
-        return button
-    }()
+        $0.layer.shadowColor = UIColor.black.cgColor
+        $0.layer.masksToBounds = false
+        $0.layer.shadowOffset = CGSize(width: 0, height: 4)
+        $0.layer.shadowOpacity = 0.3
+    }
     
 
     
@@ -192,15 +196,33 @@ class onboard3: UIView{
     func singinLink(){
         let storyboard = UIStoryboard(name: "SignIn", bundle: nil)
         let signin = storyboard.instantiateViewController(identifier: "SignInView")
-        
-        //self.present(signin, animated: true)
     }
     
     
     private func commonInit(){
-        
         self.addSubview(title)
-        self.addSubview(signinButton)
+        title.snp.makeConstraints{
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(70)
+            $0.width.equalTo(300)
+            $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
+            $0.height.equalTo(50)
+        }
+        
         self.addSubview(signupButton)
+        signupButton.snp.makeConstraints{
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.bottom).offset(-210)
+            $0.width.equalTo(self.safeAreaLayoutGuide.snp.width).multipliedBy(0.85)
+            $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
+            $0.height.equalTo(40)
+        }
+        
+        self.addSubview(signinButton)
+        signinButton.snp.makeConstraints{
+            $0.top.equalTo(signupButton.snp.bottom).offset(10)
+            $0.width.equalTo(signupButton.snp.width)
+            $0.centerX.equalTo(signupButton.snp.centerX)
+            $0.height.equalTo(signupButton.snp.height)
+        }
+
     }
 }
--- ios/Topas/Topas/ViewController/OnBoard/OnBoardViewController.swift
+++ ios/Topas/Topas/ViewController/OnBoard/OnBoardViewController.swift
@@ -6,33 +6,21 @@
 //
 import UIKit
 
-class OnBoarding: UIViewController, UIScrollViewDelegate {
+class OnBoarding: UIViewController {
     
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
 
     // View 가져오기
-    let onboardone : onboard1 = {
-        let view = onboard1()
-        //view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
+    let onboardone = onboard1()
     
-    let onboardtwo : onboard2 = {
-        let view = onboard2()
-        //view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
+    let onboardtwo = onboard2()
     
-    let onboardthree : onboard3 = {
-        let view = onboard3()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.signupButton.addTarget(self, action: #selector(signUpLink), for: .touchUpInside)
-        view.signinButton.addTarget(self, action: #selector(signInLink), for: .touchUpInside)
-        
-        return view
-    }()
+    let onboardthree = onboard3().then{
+        $0.signupButton.addTarget(self, action: #selector(signUpLink), for: .touchUpInside)
+        $0.signinButton.addTarget(self, action: #selector(signInLink), for: .touchUpInside)
+    }
     
     @objc func signUpLink(){
         self.performSegue(withIdentifier: "toSignUp", sender: self)
@@ -50,14 +38,23 @@ class OnBoarding: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        scrollView.delegate = self
         view.backgroundColor = UIColor(named: "BrandColor")
         //가져온 View 배열을 scroll view에 넣어 준다.
+        scrollView.contentSize.width = self.view.frame.width * CGFloat(3)
         for i in 0..<onboards.count{
             let onbaord = onboards[i]
             var xPosition = self.view.frame.width * CGFloat(i)
-            onbaord.frame = CGRect(x: xPosition, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-            scrollView.contentSize.width = self.view.frame.width * CGFloat(1 + i)
             scrollView.addSubview(onbaord)
+            onbaord.snp.makeConstraints{
+                $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+                $0.leading.equalTo(xPosition)
+                $0.height.equalTo(self.view.safeAreaLayoutGuide.snp.height)
+                $0.width.equalTo(self.view.safeAreaLayoutGuide.snp.width)
+            }
+            //onbaord.frame = CGRect(x: xPosition, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            //scrollView.contentSize.width = self.view.frame.width * CGFloat(1 + i)
+            
         }
         setPageControl()
     }
@@ -69,17 +66,16 @@ class OnBoarding: UIViewController, UIScrollViewDelegate {
         pageControl.pageIndicatorTintColor = .lightGray
         pageControl.currentPageIndicatorTintColor = .black
     }
-    
-    // 파라미터 값을 넘겨준 값을 페이지 컨트롤러 값으로 변경
-    private func setPageControlSelectedPage(currentPage : Int){
-        pageControl.currentPage = currentPage
-    }
-    
-    // 스크롤뷰 터치 시 호출 왜 안될까?
+}
+
+extension OnBoarding: UIScrollViewDelegate {
+    // 스크롤뷰 터치 시 호출
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let value = scrollView.contentOffset.x / scrollView.frame.size.width
         setPageControlSelectedPage(currentPage: Int(round(value)))
     }
-    
-    
+    // 파라미터 값을 넘겨준 값을 페이지 컨트롤러 값으로 변경
+    private func setPageControlSelectedPage(currentPage : Int){
+        pageControl.currentPage = currentPage
+    }
 }
